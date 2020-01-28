516caf790382e4cebf32dbb003e4a10bbadbc9dd
Remo Lazazzera <remo.tk@gmail.com>
2020-01-28 22:11:12 +0100

    First Commit

--- 20-VirtualFunctionsInCpp/20main1.cpp
+++ 20-VirtualFunctionsInCpp/20main1.cpp
@@ -40,5 +40,10 @@ int main()
     Entity* entity = p;
     std::cout << entity->GetName() << std::endl;
 
+    /* Prblems come when we use POLYMORPHISM
+    entity in this case should be a player and we should get Cherno as name 
+    instead we get Entity in the console
+    */
+
     std::cin.get();
 }
--- 20-VirtualFunctionsInCpp/20main2.cpp
+++ 20-VirtualFunctionsInCpp/20main2.cpp
@@ -41,5 +41,19 @@ int main()
     Player* p = new Player("Cherno");
     PrintName(p);
 
+    /*
+
+    Here I should expect to get in the console "Entity" and "Cherno"
+    instead I get "Entity" and "Entity".
+
+    If we want to override a function you have to mark 
+    the base function in the base class as virtual
+
+    Virtual functions do something called Dynamic Dispatch which compiles a V table.
+    A V table is a table that contains a mapping for all the virtual functions
+    of our base class so that we can map them to the correct overwrite function at runtime.
+
+    */
+
     std::cin.get();
 }
