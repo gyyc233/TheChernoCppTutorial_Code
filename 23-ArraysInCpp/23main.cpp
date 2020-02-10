07d048b0120fa2d60b924dc4276a7626d643fb3e
Remo Lazazzera <remo.tk@gmail.com>
2020-02-10 21:36:57 +0100

    First Commit

--- 23-ArraysInCpp/23main.cpp
+++ 23-ArraysInCpp/23main.cpp
@@ -7,13 +7,16 @@ int main(){
 	int example[5]; // I eclared an array of 5 elements
 	example[0] = 2; // The index tart with 0
 
-	/*
-	An array is just a pointer to the block of memory that contains 
-	the 5 integers. 
-	Array declaration allocates memory in adjacent way.
-	*/
 
-	int* ptr = example; // Im alowed to write this because an array is just a pointer
+	int* ptr = example; // Im alowed to write this and I get the pointer to the array
+						// when I print an array I get its memory address +++
+
+	/*
+		BUT BE AWARE that :
+		int count = sizeof(example);
+		is DIFFERENT from 
+		int count1 = sizeof(ptr);
+	*/
 
 	example[2] = 5;
 	*(ptr + 2) = 5; // This is an equivalent writing
@@ -22,7 +25,7 @@ int main(){
 	ptr 8 bytes because every int is 8 bytes.
 
 	If ptr was a char (1 bytes), to get to the second index position of the array
-	I would add 8 lie this:
+	I would add 8 like this:
 	*/
 	*(int*)((char*)ptr + 8) = 5; // I cast ptr to be char, then I cast ptr to int to assign 5
 
@@ -42,7 +45,6 @@ int main(){
 
 	std::cout << example[2] << std::endl;
 	std::cout << example << std::endl; // It shows the memory address of the first byte of the array
-	// because arrays are pointers.
 
 	return 0;
 }
