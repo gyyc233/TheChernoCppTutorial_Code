8332258bc89dc84141050837db5e6418d21d5668
Remo Lazazzera <remo.tk@gmail.com>
2020-02-14 23:29:01 +0100

    First Commit

--- 27-TheMutableKeywordInCpp/27exercise.cpp
+++ 27-TheMutableKeywordInCpp/27exercise.cpp
@@ -6,6 +6,31 @@ class Entity{
 	private:
 		std::string m_Name = "Hello!";
 	public:
+		std::string& GetName(){
+			m_Name = "Hi!";
+			return m_Name;
+		}
+};
+
+int main (){
+
+	Entity e;
+	std::string str = e.GetName();
+	str = "Bye";
+	std::cout<< "---Starting Program---" << std::endl;
+	std::cout<< str << std::endl;
+
+	return 0;
+}
+
+// --------------------------------------------
+#include <iostream>
+#include <string>
+
+class Entity{
+	private:
+		std::string m_Name = "Hello!";
+	public:
 		const std::string& GetName(){
 			m_Name = "Hi!";
 			return m_Name;
@@ -48,6 +73,33 @@ int main (){
 	return 0;
 }
 
+// --------------------------------------------
+#include <iostream>
+#include <string>
+
+class Entity{
+	private:
+		std::string m_Name = "Hello!";
+	public:
+		const std::string& GetName() const {
+			// m_Name = "Hi!"; 
+			return m_Name;
+		}
+};
+
+int main (){
+
+	Entity e;
+	std::string str = e.GetName();
+	str = "Bye";
+	std::cout<< "---Starting Program---" << std::endl;
+	std::cout<< str << std::endl;
+
+	return 0;
+}
+
+// --------------------------------------------
+// --------------------------------------------
 // --------------------------------------------
 #include <iostream>
 #include <string>
